67cabca59591fbefcd97ec1f548f6281ad81dbb9
Ledovv <mihailkicelev3@gmail.com>
2024-10-12 12:30:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
+#  .___                \       _____           .   
+#  /   `    __.  .___  |   ,  (      ` , __   _/_  
+#  |    | .'   \ /   \ |  /    `--.  | |'  `.  |   
+#  |    | |    | |   ' |-<        |  | |    |  |   
+#  /---/   `._.' /     /  \_ \___.'  / /    |  \__/
+
+# GitHub - https://github.com/vertex-coder/DorkSint
+
 from setuptools import setup, find_packages
+
 
 setup(
     name='dorksint',
-    version='1.0.1',
+    version='1.0.3',
     packages=find_packages(),
     py_modules=['dorksint'],
     install_requires=[
         'requests',
         'beautifulsoup4',
-        'termcolor',
+        'termcolor'
     ],
     entry_points={
         'console_scripts': [
