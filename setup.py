d31f01f4fa726e45c99fed3455a74d1ff7d59dfa
Ledovv <mihailkicelev3@gmail.com>
2024-10-18 17:43:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dorksint',
-    version='1.0.3',
+    version='1.0.5',
     packages=find_packages(),
     py_modules=['dorksint'],
     install_requires=[
